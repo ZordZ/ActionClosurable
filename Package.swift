5eb373c32f4d389510dcf8a228c8d828aa8ffe4a
MrTrent <show6time@gmail.com>
2024-06-04 02:58:52 +0300

    ios version fix + spm version fix

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ActionClosurable",
+    platforms: [.iOS("13.0")],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
