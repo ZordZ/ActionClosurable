a26e385c87de45820b5ecbb4a5f675528bee4e75
MrTrent <show6time@gmail.com>
2024-06-04 05:34:51 +0300

    remove comment

--- ActionClosurableDemo/ActionClosurableDemo/DemoViewController.swift
+++ ActionClosurableDemo/ActionClosurableDemo/DemoViewController.swift
@@ -11,7 +11,6 @@ class DemoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         configure()
     }
     
